a7e55341a386de42c5a1007bd83ca750e9bd3797
samme20 <samme.sundh@hotmail.se>
2021-12-14 16:53:06 +0100

    added whitelist

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -1,10 +1,11 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>PSYCE SMP | Home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -14,19 +15,21 @@ export default function Home() {
         </h1>
 
         <p className="description">
-          The currently number #1 minecraft SMP!
+          The currently #1 minecraft SMP!
         </p>
         
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
+          <a href="https://discord.gg/3RBTtStj4k" target="__blank" className="card">
             <h3>Discord &rarr;</h3>
             <p>Be a part of out great community.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className="card">
+        <Link href="/whitelist">
+          <a className="card">
             <h3>Register for whitelist &rarr;</h3>
             <p>View rules and apply for whitelist.</p>
           </a>
+        </Link>
 
         </div>
 
@@ -66,21 +69,9 @@ export default function Home() {
         footer {
           width: 100%;
           height: 100px;
-          border-top: 1px solid #eaeaea;
           display: flex;
           justify-content: center;
           align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          cursor: pointer;
         }
 
         a {
@@ -92,7 +83,6 @@ export default function Home() {
           color: #0070f3;
           text-decoration: none;
         }
-
 
         .title {
           margin: 0;
@@ -108,15 +98,6 @@ export default function Home() {
         .description {
           line-height: 1.5;
           font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
         }
 
         .grid {
@@ -138,10 +119,11 @@ export default function Home() {
           text-decoration: none;
           border: 1px solid #eaeaea;
           box-shadow: 3px 3px 5px 0.1 black;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
+          border-radius: 5px;
+          transition: color 0.1s ease, border-color 0.1s ease;
         }
 
+        
         .card:hover,
         .card:focus,
         .card:active {
@@ -160,14 +142,21 @@ export default function Home() {
           line-height: 1.5;
         }
 
-        .logo {
-          height: 1em;
-        }
-
         @media (max-width: 600px) {
+          .container {
+            display: flex;
+            flex-direction: column;
+            justify-content: flex-start;
+            align-items: center;
+          }
           .grid {
             width: 100%;
             flex-direction: column;
+          }
+          .title {
+            margin: 0;
+            line-height: 1.15;
+            font-size: 3rem;
           }
         }
       `}</style>
---
+++ client/pages/whitelist.jsx
@@ -0,0 +1,201 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+export default function whitelist() {
+    
+    const registerUsername = e => {
+      e.preventDefault() // don't redirect the page
+      //alert(e.target.name.value)
+ 
+    }
+    
+    return (
+    <div className="container">
+      <Head>
+        <title>PSYCE SMP | Whitelist</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className="title">
+          Rules & terms
+        </h1>
+
+        <p className="description">
+          By requesting a whitelist you agree to the following rules:
+        </p>
+
+        <ul type="none">
+            <li className="description">
+              <code>§ You may only deprive someone of their life if you and the offendant <i>both</i> have more than <i>1</i> death.</code>
+            </li>
+            <li className="description">
+                <code>§ You may only grief somone else's belongings if it's judged aggrovated by a judge. This includes to <i>some</i> degree; stealing.</code>
+            </li>
+            <li className="description">
+                <code>§ Hacked clients aswell as anything that gives you any kind of assistance (x-ray, kill aura, etc) are strictly forbidden.</code>
+            </li>
+            <li className="description">
+                <code>§ If you're black you're suscepted to abuse from everyone.</code>
+            </li>
+        </ul>
+
+        <form onSubmit={registerUsername}>
+            <input id="name" name="name" type="text" autoComplete="none" placeholder='Minecraft Username' required />
+            <button type="submit">Apply</button>
+        </form>
+
+
+      </main>
+
+      <footer>
+        <a>
+          website by &#169;STHLM Preclarus
+        </a>
+        
+      </footer>
+
+      <style jsx>{`
+        .container {
+          min-height: 100vh;
+          padding: 0 0.5rem;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        form{
+            display: flex;
+            align-items: center;
+            margin-top: 5rem;
+            flex-direction: column;
+        }
+
+        form input{
+            font-size: 2rem;
+            border: none;
+            border-bottom: 2px solid black;
+            text-align: center;
+        }
+
+        form input:focus{
+            outline: none;
+        }
+
+        form button{
+            background-color: white; 
+            color: black; 
+            border: 2px solid #5c37bc;
+            padding: 15px 32px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            margin: 4px 2px;
+            cursor: pointer;
+        }
+
+        ul{
+            text-align: center;
+            padding: 0;
+        }
+        ul *{
+            margin-bottom: 1rem;
+        }
+
+        code {
+            background: #fafafa;
+            border-radius: 5px;
+            padding: 0.75rem;
+            font-size: 1.1rem;
+            font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+              DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+          }
+
+        footer {
+          width: 100%;
+          height: 100px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
+
+        .title a {
+          color: #0070f3;
+          text-decoration: none;
+        }
+
+        .title {
+          margin: 0;
+          line-height: 1.15;
+          font-size: 4rem;
+        }
+
+        .title,
+        .description {
+          text-align: center;
+        }
+
+        .description {
+          line-height: 1.5;
+          font-size: 1.5rem;
+        }
+
+        @media (max-width: 600px) {
+          .container {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+          }
+          main {
+            width: 90%;
+            flex: 1;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+          }
+          ul *{
+            margin-bottom: 1rem;
+        }
+          .title {
+            margin: 0;
+            line-height: 1.15;
+            font-size: 3rem;
+          }
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  )
+}
